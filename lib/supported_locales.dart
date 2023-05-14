85feed1c73ab78d210db59d7f35363c0ce172906
Helix <41438338+HLXEasy@users.noreply.github.com>
2023-05-14 11:05:45 +0200

    Feature/improve translations (#76)
    * Updated Polish translation
    
    * Updated Russian translation
    
    * Finished Spanish translation
    
    * Updated German translation
    
    * Updated German, Indonesian and Polish translation
    
    * Updated Italian translation
    
    * Updated Portuguese translation
    
    * Wiped out duplicates which differ only by upper-/lowercase
    
    Usages of uppercase strings refactored to use toUpperCase() instead
    
    * Updated all translations with current state from Transifex
    
    * Updated Italian and Polish translations
    
    * Finished Swedish translations
    
    * Updated Bulgarian, Hungarian, Dutch and Chinese translations
    
    * Added Afrikaans, Persian, Finnish, Albanian and Urdu translations
    
    * Fixed Hindi translation
    
    * Updated Bulgarian, Persian, Finnish, Hindi, Hungarian and Chinese translations
    
    * Formatted base language file
    
    * Sorted list of currently supported languages
    
    * Activated support of new locales/translations
    
    * Sorted list of currently supported languages
    
    * Simplified definition of supported languages
    
    by using enhanced enum classes
    
    * Cleanup/formatted code
    
    * Fixed handling/usage of descriptive language name
    
    * Added Afrikaans, Czech, Finnish, Persian and Urdu to language selection list
    
    * Added comment regarding order of enum entries
    
    * Revert "Wiped out duplicates which differ only by upper-/lowercase"
    
    This reverts commit 7fc8dba4
    
    * Formatted base language file
    
    * Reverted changes regarding handling of upper-/lowercase strings
    
    * Added Greek
    
    even if there's currently no translated content like with Afrikaans and Urdu
    
    * Updated translations:
    
    - German
    - Persian
    - Finnish
    - Hebrew
    - Hungarian
    - Portuguese
    - Albanian
    - Swedish
    - Chinese
    
    * Update available_language.dart zh to 中文
    
    * Update available_language.dart
    
    Fixed strings for Russian and Ukrainian languages
    
    * List Persian as endonym within list of supported languages
    
    * Updated German, Greek, Spanish, French, Dutch, Polish, Russian, Albanian, Ukrainian and Chinese
    
    * Activated Albanian translation
    
    * Finished Greek, Spanish, Hebrew, Hungarian and Albanian
    
    * Updated Bulgarian, Ukrainian and Chinese
    
    * Finished Czech and Norwegian translation
    
    * Finished Persian, Hindi, Italian and Dutch translation
    
    * Finished Greek translation
    
    * Updated Hindi and Chinese translation
    
    * Updated Bulgarian and Finnish translation
    
    * Added finished Urdu translation
    
    ---------
    
    Co-authored-by: Ben <97929891+benrod3k@users.noreply.github.com>
    Co-authored-by: azazino3 <88991407+azazino3@users.noreply.github.com>

--- lib/supported_locales.dart
+++ lib/supported_locales.dart
@@ -1,33 +1,103 @@
 import 'dart:ui';
 
 const supportedLocales = const [
+  // Base language:
   const Locale('en', 'US'), // English
-  const Locale('he', 'IL'), // Hebrew
-  const Locale('de', 'DE'), // German
-  const Locale('da'), // Danish
+
+  // Translations:
+  // (Currency-default requires country included)
+  const Locale('af'), // Afrikaans
+  const Locale('ar'), // Arabic
+  const Locale("ar", "AE"), // UAE
+  const Locale("ar", "KW"), // Kuwait
+  const Locale("ar", "SA"), // Saudi Arabia
   const Locale('bg'), // Bulgarian
+  const Locale('bn'), // Bengali
+  const Locale('ca'), // Catalan
+  const Locale('cs'), // Czech
+  const Locale("cs", "CZ"),
+  const Locale('da'), // Danish
+  const Locale("da", "DK"),
+  const Locale('de', 'DE'), // German
+  const Locale("de", "CH"),
+  const Locale("de", "AT"),
+  const Locale("de", "BE"),
+  const Locale("el", "GR"),
+  const Locale("en", "AU"),
+  const Locale("en", "CA"),
+  const Locale("en", "GB"),
+  const Locale("en", "IE"),
+  const Locale("en", "MT"),
+  const Locale("en", "NZ"),
+  const Locale("en", "TW"),
+  const Locale("en", "US"),
+  const Locale("en", "ZA"),
   const Locale('es'), // Spanish
+  const Locale("es", "AR"),
+  const Locale("es", "CL"),
+  const Locale("es", "ES"),
+  const Locale("es", "MX"),
+  const Locale("es", "VE"),
+  const Locale("et", "EE"),
+  const Locale('fa', 'IR'), // Persian
+  const Locale('fi'), // Finnish
+  const Locale("fi", "FI"),
+  const Locale('fr'), // French
+  const Locale("fr", "BE"),
+  const Locale("fr", "FR"),
+  const Locale("fr", "LU"),
+  const Locale('he', 'IL'), // Hebrew
   const Locale('hi'), // Hindi
+  const Locale("hi", "IN"),
   const Locale('hu'), // Hungarian
-  const Locale('hi'), // Hindi
+  const Locale("hu", "HU"),
   const Locale('id'), // Indonesian
+  const Locale("id", "ID"),
   const Locale('it'), // Italian
+  const Locale("it", "IT"),
   const Locale('ja'), // Japanese
+  const Locale("ja", "JP"),
   const Locale('ko'), // Korean
+  const Locale("ko", "KR"),
+  const Locale("lt", "LT"),
+  const Locale('lv'), // Latvian
+  const Locale("lv", "LV"),
   const Locale('ms'), // Malay
   const Locale('nl'), // Dutch
+  const Locale("nl", "BE"),
+  const Locale("nl", "NL"),
+  const Locale('no'), // Norwegian
+  const Locale("no", "NO"), // Norway
   const Locale('pl'), // Polish
+  const Locale("pl", "PL"),
   const Locale('pt'), // Portugese
+  const Locale("pt", "BR"),
+  const Locale("pt", "PT"),
   const Locale('ro'), // Romanian
   const Locale('ru'), // Russian
+  const Locale("ru", "RU"),
+  const Locale("sk", "SK"),
   const Locale('sl'), // Slovenian
+  const Locale("sl", "SI"),
+  const Locale('sq'), // Albanian
   const Locale('sv'), // Swedish
+  const Locale("sv", "SE"),
+  const Locale("ta", "MY"),
+  const Locale("th", "TH"),
   const Locale('tl'), // Tagalog
+  const Locale("tl", "PH"),
   const Locale('tr'), // Turkish
-  const Locale('vi'), // Vietnamese
-  const Locale('ca'), // Catalan
+  const Locale("tr", "CY"),
+  const Locale("tr", "TR"),
   const Locale('uk'), // Ukrainian
-  const Locale('no'), // Norwegian
+  const Locale("uk", "UA"), // Ukraine
+  const Locale('ur'), // Urdu
+  const Locale("ur", "PK"),
+  const Locale('vi'), // Vietnamese
+  const Locale('zh'), // Chinese
+  const Locale("zh", "CN"),
+  const Locale("zh", "HK"),
+  const Locale("zh", "SG"),
   const Locale.fromSubtags(
     languageCode: 'zh',
     scriptCode: 'Hans',
@@ -36,68 +106,4 @@ const supportedLocales = const [
     languageCode: 'zh',
     scriptCode: 'Hant',
   ), // Chinese Traditional
-  const Locale('ar'), // Arabic
-  const Locale('lv'), // Latvian
-  // Currency-default requires country included
-  const Locale("es", "AR"),
-  const Locale("en", "AU"),
-  const Locale("pt", "BR"),
-  const Locale("en", "CA"),
-  const Locale("de", "CH"),
-  const Locale("es", "CL"),
-  const Locale("zh", "CN"),
-  const Locale("cs", "CZ"),
-  const Locale("da", "DK"),
-  const Locale("fr", "FR"),
-  const Locale("en", "GB"),
-  const Locale("zh", "HK"),
-  const Locale("hu", "HU"),
-  const Locale("id", "ID"),
-  const Locale("he", "IL"),
-  const Locale("hi", "IN"),
-  const Locale("ja", "JP"),
-  const Locale("ko", "KR"),
-  const Locale("es", "MX"),
-  const Locale("ta", "MY"),
-  const Locale("en", "NZ"),
-  const Locale("tl", "PH"),
-  const Locale("ur", "PK"),
-  const Locale("pl", "PL"),
-  const Locale("ru", "RU"),
-  const Locale("sv", "SE"),
-  const Locale("zh", "SG"),
-  const Locale("th", "TH"),
-  const Locale("tr", "TR"),
-  const Locale("en", "TW"),
-  const Locale("es", "VE"),
-  const Locale("en", "ZA"),
-  const Locale("en", "US"),
-  const Locale("es", "AR"),
-  const Locale("de", "AT"),
-  const Locale("fr", "BE"),
-  const Locale("de", "BE"),
-  const Locale("nl", "BE"),
-  const Locale("tr", "CY"),
-  const Locale("et", "EE"),
-  const Locale("fi", "FI"),
-  const Locale("fr", "FR"),
-  const Locale("el", "GR"),
-  const Locale("es", "AR"),
-  const Locale("en", "IE"),
-  const Locale("it", "IT"),
-  const Locale("es", "AR"),
-  const Locale("lv", "LV"),
-  const Locale("lt", "LT"),
-  const Locale("fr", "LU"),
-  const Locale("en", "MT"),
-  const Locale("nl", "NL"),
-  const Locale("pt", "PT"),
-  const Locale("sk", "SK"),
-  const Locale("sl", "SI"),
-  const Locale("es", "ES"),
-  const Locale("ar", "AE"), // UAE
-  const Locale("ar", "SA"), // Saudi Arabia
-  const Locale("ar", "KW"), // Kuwait
-  const Locale("uk", "UA"), // Ukraine
-  const Locale("no", "NO"), // Norway
 ];
