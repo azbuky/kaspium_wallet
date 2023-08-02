d118b5fd8ec583543424d7d505707c1ee4b9e306
Azbuky <azbuky@gmail.com>
2023-08-02 12:44:44 +0300

    Fix decimal input for some locales

--- lib/util/formatters/currency_formatter.dart
+++ lib/util/formatters/currency_formatter.dart
@@ -42,13 +42,21 @@ class CurrencyFormatter extends TextInputFormatter {
     TextEditingValue oldValue,
     TextEditingValue newValue,
   ) {
+    String workingText = newValue.text;
     // Only allow digits and separators
-    final inputSymbols = newValue.text.split('');
+    final inputSymbols = workingText.split('');
     if (!symbols.containsAll(inputSymbols)) {
-      return oldValue;
+      if (decimalSeparator == ',' && workingText.endsWith('.')) {
+        workingText =
+            workingText.replaceRange(workingText.length - 1, null, ',');
+      } else if (decimalSeparator == '.' && workingText.endsWith(',')) {
+        workingText =
+            workingText.replaceRange(workingText.length - 1, null, '.');
+      } else {
+        return oldValue;
+      }
     }
 
-    String workingText = newValue.text;
     // Workaround for iOS Number Keyboard missmatch
     if (workingText.endsWith(groupSeparator)) {
       workingText = workingText.substring(
