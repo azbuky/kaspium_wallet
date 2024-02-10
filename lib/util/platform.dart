adf255bd82d21a82b47751a86b4799f97ed25e94
Azbuky <azbuky@gmail.com>
2024-02-10 22:26:37 +0200

    Disable inactive overlay on macOS debug mode

--- lib/util/platform.dart
+++ lib/util/platform.dart
@@ -5,3 +5,4 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 const kPlatformIsWeb = kIsWeb;
 final kPlatformIsAndroid = !kIsWeb && Platform.isAndroid;
 final kPlatformIsIOS = !kIsWeb && Platform.isIOS;
+final kPlatformIsMacOS = !kIsWeb && Platform.isMacOS;
