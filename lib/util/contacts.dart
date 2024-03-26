2fb82a1d333b1a76490ca808181b60cc0c068ca2
Azbuky <azbuky@gmail.com>
2024-03-26 13:16:33 +0200

    Improve file name format

--- lib/util/contacts.dart
+++ lib/util/contacts.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -29,8 +30,8 @@ Future<void> exportContacts(WidgetRef ref, BuildContext context) async {
     jsonList.add(contact.toJson());
   });
   final exportTime = DateTime.now();
-  final filename =
-      "kaspium_contacts_${exportTime.year}${exportTime.month}${exportTime.day}${exportTime.hour}${exportTime.minute}${exportTime.second}.txt";
+  final format = DateFormat('yyyyMMdd_HHmmss');
+  final filename = "kaspium_contacts_${format.format(exportTime)}.txt";
   final baseDirectory = await getTemporaryDirectory();
   final contactsFile = File("${baseDirectory.path}/$filename");
   await contactsFile.writeAsString(json.encode(jsonList));
