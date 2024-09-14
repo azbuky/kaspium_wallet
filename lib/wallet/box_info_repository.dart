fbdb8802bdbf8c234b63ffe34df8674573567304
Iulian <iulian.blajut@gmail.com>
2024-09-15 00:27:05 +0300

    Fix networkId key

--- lib/wallet/box_info_repository.dart
+++ lib/wallet/box_info_repository.dart
@@ -85,7 +85,7 @@ extension BoxInfoExtension on SettingsRepository {
         byNetworkId: mainnet != null
             ? {
                 ...bundle.byNetworkId,
-                kKaspaNetworkMainnet: mainnet,
+                kKaspaNetworkIdMainnet: mainnet,
               }
             : bundle.byNetworkId,
         wasMigrated: true,
