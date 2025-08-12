8bd386eeba93cdcdfd7f65cb39f2f7306178fa9d
Azbuky <azbuky@gmail.com>
2025-08-12 15:27:45 +0300

    Update Flutter version and dependencies

--- lib/util/hapticutil.dart
+++ lib/util/hapticutil.dart
@@ -1,7 +1,4 @@
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/services.dart';
-
-//import 'package:flutter_vibrate/flutter_vibrate.dart';
 
 import 'platform.dart';
 
@@ -9,56 +6,14 @@ import 'platform.dart';
 class HapticUtil {
   const HapticUtil();
 
-  /// Return true if this device supports taptic engine (iPhone 7+)
-  Future<bool> hasTapicEngine() async {
-    if (!kPlatformIsIOS) {
-      return false;
-    }
-    IosDeviceInfo deviceInfo = await DeviceInfoPlugin().iosInfo;
-    String deviceIdentifier = deviceInfo.utsname.machine;
-    switch (deviceIdentifier) {
-      case 'iPhone5,1': // iPhone 5
-      case 'iPhone5,2': // iPhone 5
-      case 'iPhone5,3': // iPhone 5C
-      case 'iPhone5,4': // iPhone 5C
-      case 'iPhone6,1': // iPhone 5S
-      case 'iPhone6,2': // iPhone 5S
-      case 'iPhone7,2': // iPhone 6
-      case 'iPhone7,1': // iPhone 6 plus
-      case 'iPhone8,1': // iPhone 6s
-      case 'iPhone8,2': // iPhone 6s plus
-        return false;
-      default:
-        return true;
-    }
-  }
-
   /// Feedback for error
   Future<void> error() async {
-    if (kPlatformIsIOS) {
-      // If this is simulator or this device doesnt have tapic then we can't use this
-      // if (await hasTapicEngine() && await Vibrate.canVibrate) {
-      //   Vibrate.feedback(FeedbackType.error);
-      // } else {
-      HapticFeedback.vibrate();
-      //}
-    } else {
-      HapticFeedback.vibrate();
-    }
+    HapticFeedback.vibrate();
   }
 
   /// Feedback for success
   Future<void> success() async {
-    if (kPlatformIsIOS) {
-      // If this is simulator or this device doesnt have tapic then we can't use this
-      // if (await hasTapicEngine() && await Vibrate.canVibrate) {
-      //   Vibrate.feedback(FeedbackType.medium);
-      // } else {
-      HapticFeedback.mediumImpact();
-      //}
-    } else {
-      HapticFeedback.mediumImpact();
-    }
+    HapticFeedback.mediumImpact();
   }
 
   /// Feedback for fingerprint success
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import app_links
-import device_info_plus
 import file_picker
 import file_selector_macos
 import flutter_secure_storage_macos
@@ -19,7 +18,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
